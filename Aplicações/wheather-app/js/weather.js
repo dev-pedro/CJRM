42ecc34cfe0960730cc66c3ee4742397323f144f
dev-pedro <dev.pedro.rjas@gmail.com>
2023-04-28 15:23:05 -0300

    resumo - Obtendo informações da cidade - Aula 01-06

--- Aplicações/wheather-app/js/weather.js
+++ Aplicações/wheather-app/js/weather.js
@@ -1,7 +1,22 @@
-const getApiKey = async () => {
-  await fetch('src/config/apiInfo.json')
-    .then(response => response.json())
-    .catch(err => err)
+const APIKey = 'G5OnpGIKXjtGnKOAvTD5r1qpDutIEQFP'
+
+const cityUrl = cityName =>
+  `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+  const ciytUrl = cityUrl(cityName)
+  try {
+    const response = await fetch(ciytUrl)
+
+    if (!response.ok) {
+      throw new Error('Não foi possível receber os dados!')
+    }
+    const [cityData] = await response.json()
+
+    return cityData
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`)
+  }
 }
-const apiInfo = getApiKey()
-console.log(apiInfo.key)
+
+console.log((getCityData('hortolandia')))
